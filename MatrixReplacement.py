e642bf9ebbde617ea3df0b6c2ff27a6d0b2abd2c
Joe Friedrich <joe.friedrich@gmail.com>
2016-11-19 17:08:56 -0500

    Single approver function created with docstrings.
    Other readability/maintenance improvements:
    -function output_to_screen_and_clipboard renamed to parse_role_tuples.
    docstring added.
    -function email_format, added docstring
    -class Company regex renamed to regular_expression, self.regex renamed to
    role_format
    -class function vacation_check docstring added
    -class function find_and_sort_roles, variable find_this renamed to
    find_role
    -main program, variable matrix renamed to company
    -rewrote some of the user instructions
    
    notes:
    possible issue:  exiting the single client functionality looks the same
    when we exit the program in other menus.
    should we create an 'exiting program' function?

--- MatrixReplacement.py
+++ MatrixReplacement.py
@@ -13,19 +13,20 @@ from collections import namedtuple
 from datetime import datetime
 from tkinter import Tk
 
-#-----------------------------------------------------------------------------
+#------------------------Begin Company Class-----------------------------------------------------
 class Company:
-	def __init__(self, name, file_name, regex, clients):
+	def __init__(self, name, file_name, regular_expression, clients):
 		self.name = name
 		self.excel_file = openpyxl.load_workbook(file_name, read_only = True)
-		self.regex = regex
+		self.role_format = regular_expression
 		self.clients = clients
 		self.vacation_lookup = self.load_vacations()
-		self.regions = self.load_regions()
+		self.region_lookup = self.load_regions()
 		self.role_lookup = self.load_roles()
 		self.email_lookup = self.load_emails()
 		self.excel_file = None
 
+		
 	def load_regions(self):
 		'''
 			This grabs the contents of the cells in the top row of the Roles worksheet.
@@ -45,6 +46,11 @@ class Company:
 
 	def vacation_check(self, names):
 		'''
+			Takes a list of strings (names)
+			For each name in names, looks for name in vacation_lookup (list of tuples).
+				If the name is there, checks the date against the dates in vacation_lookup.
+				If both of these check out, it replaces name with the name in vacation_lookup.
+			Returns a list of strings (checked_names)
 		'''	
 		checked_names = []
 		for name in names:
@@ -69,9 +75,9 @@ class Company:
 		'''
 		approvers = []
 		if row[2].value == 'Regional':
-			approvers = [approver.value for approver in row[4:4 + len(self.regions)]]
+			approvers = [approver.value for approver in row[4:4 + len(self.region_lookup)]]
 		else:
-			for approver in row[4:4 + len(self.regions)]:
+			for approver in row[4:4 + len(self.region_lookup)]:
 				if approver.value != 'N/A':
 					approvers.append(approver.value)
 					break
@@ -140,11 +146,11 @@ class Company:
 		'''
 		roles_and_approvers = []
 		for line in user_input:
-			find_this = self.regex.search(line.upper())
+			find_role = self.role_format.search(line.upper())
 			role_not_found = True
-			if(find_this != None):
+			if(find_role != None):
 				for row in self.role_lookup:
-					if find_this.group() == row.name:
+					if find_role.group() == row.name:
 						if len(row.approvers) > 1:
 							roles_and_approvers.append((row.name,
 											row.description, 
@@ -155,10 +161,11 @@ class Company:
 											row.approvers[0]))
 						role_not_found = False
 				if role_not_found:
-					print('\nRole ' + find_this.group() + ' was not found.')
+					print('\nRole ' + find_role.group() + ' was not found.')
 		return sorted(roles_and_approvers, key = lambda entry: entry[2])
 
-#-------------------------------------------------------------------------------
+		
+#---------------------------End Company Class---------------------------------------------------
 def get_menu(list_of_things):
 	'''
 		This takes a list as input.
@@ -191,14 +198,14 @@ def get_menu_input(menu_total):
 				return user_input
 			else:  #the numerical value is not a menu option
 				quitting = input('\nAre you sure you are finished?\nPress y and enter to quit.\nAny other key(s) and enter to continue:  ')
-				if quitting == 'y':  #document change!
+				if quitting == 'y':
 					return 0
 		except ValueError:  #if the value is a non-numeral or float
 			quitting = input('\nAre you sure you are finished?\nPress y and enter to quit.\nAny other key(s) and enter to continue:  ')
-			if quitting == 'y':  #document change!
+			if quitting == 'y':
 				return 0
 
-	
+				
 def get_role_input():
 	'''
 		This is just a placeholder for user input that is not in the form
@@ -206,9 +213,18 @@ def get_role_input():
 	'''
 	return stdin.readlines()
 
-
-def output_to_screen_and_clipboard(output, company, clipboard):
+	
+def parse_role_tuples(output, company, clipboard):
 	'''
+		Takes the list of tuples (output), the company object, and the clipboard.
+			Output is expected to be sorted by approver (output[2]).
+			Clipboard should be blank.
+		For every unique approver in the tuples, it will print and append a header
+			to the clipboard.
+			It will also append the email address to approver_emails.
+		These are followed by the role names until a new approver name is found or
+			the list ends.
+		Returns a list of strings (approver_emails) and the clipboard.
 	'''
 	current_approver = ''
 	approver_emails = []
@@ -221,12 +237,42 @@ def output_to_screen_and_clipboard(output, company, clipboard):
 				approver_emails.append(company.email_lookup[current_approver])
 			else:
 				approver_emails.append(current_approver + "'s email is missing")
-		print(role_tuple[0] + '\t' + role_tuple[1])
-		clipboard.clipboard_append(role_tuple[0] + '\t' + role_tuple[1] + '\n')
+		print(role_tuple[0] + '\t ' + role_tuple[1])
+		clipboard.clipboard_append(role_tuple[0] + '\t ' + role_tuple[1] + '\n')
 	return approver_emails, clipboard
 
+	
+def single_approvers(single_approver_data, email_list, clipboard):
+	'''
+		Takes list of tuples, list of strings, and clipboard.
+		Will take one single approver at a time and append it's data to 
+			email_list and clipboard.
+		Returns list of strings (email_list) and clipboard.
+	'''
+	while True:
+		print('\n************Single Approver Client Section************************')
+		print('***This will print your single client approvers UNTIL ************')
+		print('*****you make a selection that is NOT on the menu.****************')
+		print('******************************************************************')
+		print('\nDoes the user need additional access in single approver clients?')
+		select_single_client = get_menu(single_approver_data)
+		if select_single_client == 0:
+			break
+		
+		selected_client = single_approver_clients[select_single_client - 1]
+		
+		print(selected_client[1])
+		clipboard.clipboard_append(selected_client[1])
+		if selected_client[2] not in email_list:
+			email_list.append(selected_client[2])
+		
+	return email_list, clipboard
+
+	
 def email_format(email_list):
 	'''
+		Takes a list of strings.
+		Returns a concatination of this list.
 	'''
 	email_output = ''
 	for email in email_list:
@@ -235,8 +281,8 @@ def email_format(email_list):
 	print('\n' + email_output[:-2] + '\n')
 	return str('\n' + email_output[:-2] + '\n')
 	
-	
-#-------------------------------------------------------------------------------
+
+#--------------------------------End Local Functions-----------------------------------------------
 simplefilter('ignore')  #blocks some warnings that openpyxl throws when loading files
 
 print('Loading companies.')
@@ -270,17 +316,17 @@ list_single_approvers = [client[0] for client in single_approver_clients]
 clipboard = Tk()
 clipboard.withdraw() #removes Tk window that is not needed at this time.
 			
-#Begin Program
+#--------------------------------Begin Program-----------------------------------------------
 while (True):	
-	print('\nWhich company?')
+	print('\nTo which company does the user belong?')
 	select_company = get_menu(list_company_names)
 	if select_company == 0:
 		clipboard.destroy()  
 		break
-	matrix = list_companies[select_company - 1]
+	company = list_companies[select_company - 1]
 	
-	print('\nWhich region?')
-	select_region = get_menu(matrix.regions)
+	print('\nWhich region does the user wish to access?')
+	select_region = get_menu(company.region_lookup)
 	if select_region == 0:
 		clipboard.destroy()  
 		break
@@ -289,32 +335,19 @@ while (True):
 	print("\nPaste the roles in, hit Ctrl+D (Ctrl+Z and Enter for Windows).")
 	requested_roles = get_role_input()
 	
-	organized_output = matrix.find_and_sort_roles(requested_roles, region)
+	role_tuples = company.find_and_sort_roles(requested_roles, region)
 	
-	print('\nWhich client?')
-	select_client = get_menu(matrix.clients)
+	print('\nIn which SAP client does the user want the access?')
+	select_client = get_menu(company.clients)
 	if select_client == 0:
 		clipboard.destroy()  
 		break
-	user_client = matrix.clients[select_client - 1]
+	user_client = company.clients[select_client - 1]
 	
 	clipboard.clipboard_clear() #clear's the current contents of the clipboard in prep for new data
-	email_list, clipboard = output_to_screen_and_clipboard(organized_output, matrix, clipboard)
+	email_list, clipboard = parse_role_tuples(role_tuples, company, clipboard)
 	
-	while True:
-		print('\n***************Single Approver Client Section*********************')
-		print('***This will print your single client approvers UNTIL YOU STOP.***')
-		print('******************************************************************')
-		select_single_client = get_menu(list_single_approvers)
-		if select_single_client == 0:
-			break
-		
-		selected_client = single_approver_clients[select_single_client - 1]
-		
-		print(selected_client[1])
-		clipboard.clipboard_append(selected_client[1])
-		if selected_client[2] not in email_list:
-			email_list.append(selected_client[2])
+	email_list, clipboard = single_approvers(list_single_approvers, email_list, clipboard)
 	
 	clipboard.clipboard_append(email_format(email_list))
 	print('\nYour output is now in the clipboard.  Paste it into your ticket.')
